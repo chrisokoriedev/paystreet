2d7169c04fba73c88afb7e511e7e5c1d3692aea9
christian-iuil <okoriec01@gmail.com>
2024-06-03 16:49:13 +0100

    update

--- lib/util/app_string.dart
+++ lib/util/app_string.dart
@@ -3,6 +3,9 @@ class AppString {
   static const dataIcon = 'assets/icons/buydata.png';
   static const cableIcon = 'assets/icons/cable.png';
   static const eduIcon = 'assets/icons/edupay.png';
+  static const withdrwIcon = 'assets/icons/withdraw.png';
+  static const agentIcon = 'assets/icons/agent.png';
+  static const askIcon = 'assets/icons/ask4pay.png';
   static const lightIcon = 'assets/icons/light.png';
   static const shopingIcon = 'assets/icons/shop.png';
   static const transferIcon = 'assets/icons/transfer.png';
--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class AppColor{
   static const lightBlue=Color(0xffCFEDFA);
   static const lightPink=Color(0xffFACFF3);
+  static const lightYellow=Color(0xffFAF5CF);
   static const lightPurple=Color(0xffEBEBFB);
+  static const lightPurpleII=Color(0xffDDCFFA);
   static const lightGrey=Color(0xffDCDCE8);
   static const lightGreen=Color(0xffE0F4ED);
   static const lightGreenII=Color(0xffDDFACF);
+  static const lightGreenIII=Color(0xffF1FACF);
 }
